c92d832b7efa4b68b91c5c1bc9a49f3ecb22a29e
FelisPhasma <felisphasma@gmail.com>
2017-11-23 21:35:30 -0700

    Further improved uppon compactness. Added readme.

--- higgs.js
+++ higgs.js
@@ -1,17 +1,10 @@
 // Higgs.js micro selector library
 // Copyright Felis Phasma 2014-2017 v3.0
 window._ = (query, context) => {
-	context = context || document;
 	let pad = [".", "#"].indexOf(query[0]);
-	return [
-		(s) => {
-			return context.getElementsByTagName(s);
-		},
-		(s) => {
-			return context.getElementsByClassName(s);
-		},
-		(s) => {
-			return context.getElementById(s);
-		}
-	][pad + 1](pad < 0 ? query : query.slice(1));
+	return (context || document)[[
+		"getElementsByTagName",
+		"getElementsByClassName",
+		"getElementById"
+	][pad + 1]](pad < 0 ? query : query.slice(1))
 }
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Higgs.js
+
+The smallest JavaScript selector library.  
+
+152 bytes.
+
+Copyright Felis Phasma 2014-2017
